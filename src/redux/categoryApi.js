46588aee4902c9f7abbc14a50c17244f2a1b6b6f
Prateek Bahad <prateekbahad@gmail.com>
2026-09-08 09:47:29 +0530

    update categoryApi.js

---
+++ src/redux/categoryApi.js
@@ -0,0 +1,46 @@
+import { baseApi } from "./baseApi";
+
+const categoryApi = baseApi.injectEndpoints({
+  tagTypes: ["Category"],
+  endpoints: (builder) => ({
+    getAllCategory: builder.query({
+      query: () => ({
+        url: "/category/getAllCategory",
+        method: "GET",
+      }),
+      providesTags: ["Category"],
+    }),
+
+    getCategoryById: builder.query({
+      query: (id) => ({
+        url: `/category/getCategoryById/${id}`,
+        method: "GET",
+      }),
+      providesTags: ["Category"],
+    }),
+
+    updateCategoryById: builder.mutation({
+      query: (id, newData) => ({
+        url: `/category/updateCategoryById/${id}`,
+        method: "PUT",
+        body: newData,
+      }),
+      invalidatesTags: ["Category"],
+    }),
+
+    deleteCategoryById: builder.mutation({
+      query: (id) => ({
+        url: `/category/deleteCategoryById/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Category"],
+    }),
+  }),
+});
+
+export const {
+  useGetAllCategoryQuery,
+  useGetCategoryByIdQuery,
+  useUpdateCategoryByIdMutation,
+  useDeleteCategoryByIdMutation,
+} = categoryApi;
