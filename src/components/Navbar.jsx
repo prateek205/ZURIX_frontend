018e0ce6ffddf51c1c8b5d7dc511c1ea4a5af0ac
Prateek Bahad <prateekbahad@gmail.com>
2026-09-12 11:23:01 +0530

    update navbar.jsx, productPage.jsx file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { CiHeart } from "react-icons/ci";
 import { FaRegUser } from "react-icons/fa";
 import { IoIosSearch } from "react-icons/io";
@@ -7,44 +7,18 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  // Detect scrolling
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   return (
     <nav
-      className={`
-        fixed
+      className="
+        sticky
         top-0
-        left-0
         z-50
         w-full
         font-zurixFont
-        transition-all
-        duration-500
-        ease-in-out
-
-        ${
-          isScrolled
-            ? "bg-white shadow-[0_2px_15px_rgba(0,0,0,0.08)]"
-            : "bg-transparent"
-        }
-      `}
+        text-black
+        backdrop-blur-md
+      "
     >
       <div
         className="
@@ -65,104 +39,104 @@ const Navbar = () => {
         ========================== */}
 
         <ul
-          className={`
+          className="
             hidden
             items-center
             gap-6
             text-[15px]
             font-semibold
+            text-black
             md:flex
             lg:gap-9
             xl:gap-11
-
-            ${isScrolled ? "text-black" : "text-white"}
-          `}
+          "
         >
-        <Link to={"/"}>
-          <li className="group relative cursor-pointer">
-            Home
-            <span
-              className={`
-                absolute
-                -bottom-2
-                left-0
-                h-[1px]
-                w-0
-                transition-all
-                duration-300
-                group-hover:w-full
-                ${isScrolled ? "bg-black" : "bg-white"}
-              `}
-            />
-          </li>
-        </Link>
-        <Link to={"/products"}>
-          <li className="group relative cursor-pointer">
-            Shop
-            <span
-              className={`
-                absolute
-                -bottom-2
-                left-0
-                h-[1px]
-                w-0
-                transition-all
-                duration-300
-                group-hover:w-full
-                ${isScrolled ? "bg-black" : "bg-white"}
-              `}
-            />
-          </li>
-        </Link>
+          <Link to="/">
+            <li className="group relative cursor-pointer">
+              Home
+              <span
+                className="
+                  absolute
+                  -bottom-2
+                  left-0
+                  h-[1px]
+                  w-0
+                  bg-white
+                  transition-all
+                  duration-300
+                  group-hover:w-full
+                "
+              />
+            </li>
+          </Link>
+
+          <Link to="/products">
+            <li className="group relative cursor-pointer">
+              Shop
+              <span
+                className="
+                  absolute
+                  -bottom-2
+                  left-0
+                  h-[1px]
+                  w-0
+                  bg-white
+                  transition-all
+                  duration-300
+                  group-hover:w-full
+                "
+              />
+            </li>
+          </Link>
 
           <li className="group relative cursor-pointer">
             Pages
             <span
-              className={`
+              className="
                 absolute
                 -bottom-2
                 left-0
                 h-[1px]
                 w-0
+                bg-white
                 transition-all
                 duration-300
                 group-hover:w-full
-                ${isScrolled ? "bg-black" : "bg-white"}
-              `}
+              "
             />
           </li>
 
           <li className="group relative cursor-pointer">
             Blog
             <span
-              className={`
+              className="
                 absolute
                 -bottom-2
                 left-0
                 h-[1px]
                 w-0
+                bg-white
                 transition-all
                 duration-300
                 group-hover:w-full
-                ${isScrolled ? "bg-black" : "bg-white"}
-              `}
+              "
             />
           </li>
 
           <li className="group relative cursor-pointer">
             Contact Us
             <span
-              className={`
+              className="
                 absolute
                 -bottom-2
                 left-0
                 h-[1px]
                 w-0
+                bg-white
                 transition-all
                 duration-300
                 group-hover:w-full
-                ${isScrolled ? "bg-black" : "bg-white"}
-              `}
+              "
             />
           </li>
         </ul>
@@ -179,21 +153,18 @@ const Navbar = () => {
           "
         >
           <h1
-            className={`
+            className="
               cursor-pointer
               text-[28px]
               font-bold
               uppercase
               tracking-[-1px]
-              transition-colors
-              duration-500
+              text-black
               sm:text-[32px]
               md:text-[36px]
-
-              ${isScrolled ? "text-black" : "text-white"}
-            `}
+            "
           >
-            ZURIX<span className="text-[20px]">.</span>
+            ZURI<span className=" text-red-500">X.</span>
           </h1>
         </div>
 
@@ -202,20 +173,17 @@ const Navbar = () => {
         ========================== */}
 
         <ul
-          className={`
+          className="
             ml-auto
             flex
             items-center
             gap-3
             text-[22px]
-            transition-colors
-            duration-500
+            text-black
             sm:gap-4
             md:gap-5
             md:text-[24px]
-
-            ${isScrolled ? "text-black" : "text-white"}
-          `}
+          "
         >
           {/* User */}
           <li
@@ -289,18 +257,13 @@ const Navbar = () => {
           transition-all
           duration-500
           md:hidden
-
           ${isMenuOpen ? "max-h-[400px] border-t" : "max-h-0"}
-
-          ${
-            isScrolled
-              ? "border-black/10 bg-white"
-              : "border-white/20 bg-black/20 backdrop-blur-md"
-          }
+          border-white/20
+          bg-black
         `}
       >
         <ul
-          className={`
+          className="
             flex
             flex-col
             items-center
@@ -308,9 +271,8 @@ const Navbar = () => {
             py-7
             text-[16px]
             font-semibold
-
-            ${isScrolled ? "text-black" : "text-white"}
-          `}
+            text-white
+          "
         >
           <li className="cursor-pointer" onClick={() => setIsMenuOpen(false)}>
             Home
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -4,11 +4,31 @@ import { useGetProductsQuery } from "../redux/productApi";
 const ProductsPage = () => {
   const { data, isLoading, isError } = useGetProductsQuery();
 
+  if (isLoading) {
+    return <p>Loading the Product...</p>;
+  }
+
+  if (isError) {
+    return <p>Unable to fetch data...</p>;
+  }
+
   const products = data?.data || [];
 
   console.log("PRODUCTS:", products);
 
-  return <div>ProductsPage</div>;
+  return (
+    <section className="h-full p-10 mt-4">
+      <div>
+        <img src="./shop/hero_banner_shop.jpg" alt="" />
+      </div>
+
+      {products.map((product) => (
+        <div key={product._id}>
+          <img src={product.category.image} alt="" />
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default ProductsPage;
