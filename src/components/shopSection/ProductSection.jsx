b6f9ec1655dd787a41fe14af9433edb031a66a40
Prateek Bahad <prateekbahad@gmail.com>
2026-09-14 16:38:23 +0530

    update the productSection.js

--- src/components/shopSection/ProductSection.jsx
+++ src/components/shopSection/ProductSection.jsx
@@ -21,41 +21,56 @@ const ProductSection = () => {
 
   return (
     <section className="p-2 grid grid-cols-3 gap-2">
-      <div
-        className="w-[100%] h-auto p-2 bg-[rgb(91,91,91,.1)] rounded-xl flex flex-col gap-5"
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        <div className="relative overflow-hidden w-[100%] h-96 rounded-md flex items-center justify-center">
-          <img
-            src="images/category/womens/women-1.jpg"
-            alt=""
-            className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-500 ease-in-out 
-              ${isHovered ? "opacity-0" : "opacity-100"}`}
-          />
-          <img
-            src="images/category/womens/women-2.jpg"
-            alt=""
-            className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-500 ease-in-out 
-              ${isHovered ? "opacity-100" : "opacity-0"}`}
-          />
-
-          <button
-            className={`absolute top-4 flex-col gap-5 right-4 flex items-center justify-center transition-all duration-300 ${
-              isHovered
-                ? "opacity-100 translate-y-0"
-                : "opacity-0 -translate-y-2"
-            }`}
+      {products.map((product) => {
+        return (
+          <div
+            key={product._id}
+            className="w-[100%] h-auto p-2 bg-[rgb(91,91,91,.1)] rounded-xl flex flex-col gap-5"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
           >
-            <LuEye className="text-2xl w-7 h-7 rounded-full border border-black bg-white" />
-            <CiHeart className="text-sm w-6 h-6 rounded-full bg-white" />
-          </button>
-        </div>
-        <div className="flex flex-col gap-2">
-          <h1 className="font-bold text-xl">Basic blazer</h1>
-          <p className="text-lg">$199.99</p>
-        </div>
-      </div>
+            <div className="relative overflow-hidden w-[100%] h-96 rounded-md flex items-center justify-center">
+              <img
+                src={product.images[0].url}
+                alt=""
+                className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-500 ease-in-out 
+              ${isHovered ? "opacity-0" : "opacity-100"}`}
+              />
+              <img
+                src={product.images[1].url}
+                alt=""
+                className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-500 ease-in-out 
+              ${isHovered ? "opacity-100" : "opacity-0"}`}
+              />
+
+              <button
+                className={`absolute top-4 flex-col gap-5 right-4 flex items-center justify-center transition-all duration-300 ${
+                  isHovered
+                    ? "opacity-100 translate-y-0"
+                    : "opacity-0 -translate-y-2"
+                }`}
+              >
+                <LuEye className="text-2xl w-7 h-7 rounded-full bg-white" />
+                <CiHeart className="text-sm w-7 h-7 rounded-full bg-white" />
+              </button>
+
+              <button
+                className={`absolute bottom-4 left-1/2 -translate-x-1/2 w-[90%] py-3 bg-black text-white rounded-full font-medium transition-all duration-300 hover:bg-orange-600 hover:text-white ${
+                  isHovered
+                    ? "opacity-100 translate-y-0"
+                    : "opacity-0 translate-y-4"
+                }`}
+              >
+                Select Options
+              </button>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1 className="font-bold text-xl">{product.name}</h1>
+              <p className="text-lg">${product.price}</p>
+            </div>
+          </div>
+        );
+      })}
     </section>
   );
 };
