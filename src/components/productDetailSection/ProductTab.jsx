70aa03b711396ddc7d4387a5ad4929a4b063cf50
Prateek Bahad <prateekbahad@gmail.com>
2026-09-16 18:02:53 +0530

    update src/components/productDetailSection/productTab.jsx, modify- src/pages/productDetails.jsx

---
+++ src/components/productDetailSection/ProductTab.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductTab = () => {
+  return (
+    <div>ProductTab</div>
+  )
+}
+
+export default ProductTab
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,6 +3,7 @@ import ImageSection from "../components/productDetailSection/ImageSection";
 import ProductInfo from "../components/productDetailSection/ProductInfo";
 import { useGetProductByIdQuery } from "../redux/productApi";
 import { useParams } from "react-router-dom";
+import ProductTab from "../components/productDetailSection/ProductTab";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -22,7 +23,7 @@ const ProductDetails = () => {
   console.log("PRODUCT_DATA:", product);
 
   return (
-    <section className="flex items-center w-[95%] m-auto py-[28px] mt-10">
+    <section className="flex items-center flex-col gap-5 w-[95%] m-auto py-[28px] mt-10">
       <div className="w-[100%] p-5 flex gap-5">
         <div className="w-[50%] p-2">
           <ImageSection product={product} />
@@ -30,6 +31,9 @@ const ProductDetails = () => {
         <div className="w-[50%] border border-black p-2">
           <ProductInfo product={product} />
         </div>
+      </div>
+      <div>
+        <ProductTab />
       </div>
     </section>
   );
