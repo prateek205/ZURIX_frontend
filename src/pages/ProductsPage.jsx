d4b1da1aef5af292784468ddac4a3c807b7ec649
Prateek Bahad <prateekbahad@gmail.com>
2026-09-16 22:17:20 +0530

    update the src/pages/productPage.jsx

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import FilterSection from "../components/shopSection/FilterSection";
 import SortSection from "../components/shopSection/SortSection";
 import ProductSection from "../components/shopSection/ProductSection";
@@ -16,22 +17,104 @@ const ProductsPage = () => {
   });
 
   return (
-    <section className="h-full">
-      {/* ================= HERO ================= */}
-      <div className="mt-14 pb-2">
+    <section className="h-full w-full">
+      {/* =================================================
+          HERO
+      ================================================== */}
+
+      <div
+        className="
+          mt-[20px]
+          pb-[5px]
+          sm:mt-[28px]
+          sm:pb-[8px]
+          md:mt-[35px]
+          lg:mt-[55px]
+        "
+      >
         <img
           src="images/shop/hero_banner_shop.jpg"
-          alt=""
-          className="w-[87%] m-auto h-[400px] rounded-[20px] object-cover"
+          alt="Shop"
+          className="
+            mx-auto
+            h-[220px]
+            w-[92%]
+            rounded-[12px]
+            object-cover
+
+            sm:h-[280px]
+            sm:w-[90%]
+            sm:rounded-[15px]
+
+            md:h-[340px]
+            md:w-[89%]
+            md:rounded-[18px]
+
+            lg:h-[400px]
+            lg:w-[87%]
+            lg:rounded-[20px]
+          "
         />
       </div>
 
-      {/* ================= SHOP SECTION ================= */}
-      <div className="w-[88%] m-auto p-5">
-        <div className="flex w-full gap-3">
-          {/* ================= FILTER ================= */}
-          <div className="w-[25%] py-1 px-2">
-            <div className="sticky top-[20px]">
+      {/* =================================================
+          SHOP SECTION
+      ================================================== */}
+
+      <div
+        className="
+          mx-auto
+          w-[94%]
+          py-[20px]
+
+          sm:w-[92%]
+          sm:py-[25px]
+
+          md:w-[90%]
+          md:py-[30px]
+
+          lg:w-[88%]
+          lg:py-[35px]
+        "
+      >
+        <div
+          className="
+            flex
+            w-full
+            flex-col
+            gap-[20px]
+
+            md:gap-[25px]
+
+            lg:flex-row
+            lg:gap-[15px]
+          "
+        >
+          {/* =================================================
+              FILTER
+          ================================================== */}
+
+          <div
+            className="
+              w-full
+              px-0
+              py-0
+
+              md:px-[5px]
+
+              lg:w-[25%]
+              lg:px-[8px]
+              lg:py-1
+            "
+          >
+            <div
+              className="
+                w-full
+
+                lg:sticky
+                lg:top-[20px]
+              "
+            >
               <FilterSection
                 queryParams={queryParams}
                 setQueryParams={setQueryParams}
@@ -39,18 +122,40 @@ const ProductsPage = () => {
             </div>
           </div>
 
-          {/* ================= PRODUCTS ================= */}
-          <div className="w-[75%] flex flex-col gap-3">
-            {/* Sort */}
-            <div>
+          {/* =================================================
+              PRODUCTS
+          ================================================== */}
+
+          <div
+            className="
+              flex
+              w-full
+              min-w-0
+              flex-col
+              gap-[8px]
+
+              md:gap-[12px]
+
+              lg:w-[75%]
+              lg:gap-3
+            "
+          >
+            {/* =================================================
+                SORT
+            ================================================== */}
+
+            <div className="w-full">
               <SortSection
                 queryParams={queryParams}
                 setQueryParams={setQueryParams}
               />
             </div>
 
-            {/* Products */}
-            <div>
+            {/* =================================================
+                PRODUCT GRID
+            ================================================== */}
+
+            <div className="w-full min-w-0">
               <ProductSection queryParams={queryParams} />
             </div>
           </div>
