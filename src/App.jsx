6b367466fe587f57bce696619b2bec56eadc1ff7
Prateek Bahad <prateekbahad@gmail.com>
2026-09-18 10:54:54 +0530

    update the protectedroutes.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Route, Routes } from "react-router-dom";
 import ProductsPage from "./pages/ProductsPage";
 import Footer from "./components/Footer";
 import ProductDetails from "./pages/ProductDetails";
+import CartPage from "./pages/CartPage";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
 
 const App = () => {
   return (
@@ -14,6 +16,14 @@ const App = () => {
         <Route path="/" element={<HeroPage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/productDetail/:id" element={<ProductDetails />} />
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoutes>
+              <CartPage />
+            </ProtectedRoutes>
+          }
+        />
       </Routes>
       <Footer />
     </>
---
+++ src/utils/ProtectedRoutes.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useGetProfileQuery } from "../redux/authApi";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoutes = ({ children }) => {
+  const { data, isLoading, isError } = useGetProfileQuery();
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-cneter h-screen">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (!isError || !data?.success) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoutes;
