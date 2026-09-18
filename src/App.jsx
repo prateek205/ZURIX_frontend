baa7dee5fd70d4280c71db196e50f59fe2c65b8b
Prateek Bahad <prateekbahad@gmail.com>
2026-09-18 18:00:10 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 import ProductDetails from "./pages/ProductDetails";
 import CartPage from "./pages/CartPage";
 import ProtectedRoutes from "./utils/ProtectedRoutes";
-import Login from "./pages/loginPage";
+import Login from "./pages/LoginPage";
 import Profile from "./pages/Profile";
 
 const App = () => {
