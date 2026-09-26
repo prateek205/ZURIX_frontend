24949552ef6331444cec29ca5bee3a0176623ff7
Prateek Bahad <prateekbahad@gmail.com>
2026-09-26 17:29:45 +0530

    update the wishlistSection.jsx

---
+++ src/pages/WishlistSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Wishlist from "../components/Wishlist";
+
+const WishlistSection = () => {
+  return (
+    <section>
+      <Wishlist />
+    </section>
+  );
+};
+
+export default WishlistSection;
